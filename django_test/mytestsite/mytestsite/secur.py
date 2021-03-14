f00f61a75447f7df483230fbfedcd63436749e5a
predator2508 <citroen8897@gmail.com>
2021-03-14 03:11:21 +0200

    update 14.03 03:11

--- django_test/mytestsite/mytestsite/secur.py
+++ django_test/mytestsite/mytestsite/secur.py
@@ -1,4 +1,6 @@
 def secur_x(string):
-    for j in ['delete', 'insert', 'set', 'update', 'drop']:
-        if j in string.lower():
-            return 0
+    if string is not None:
+        if string.isalpha():
+            for j in ['delete', 'insert', 'set', 'update', 'drop']:
+                if j in string.lower():
+                    return 0
