6dc18064ae350355dca117109bd42881831ea276
predator2508 <citroen8897@gmail.com>
2021-05-15 01:28:59 +0300

    update 15.05 01:28

---
+++ test/simpledms/documents/travail_methods.py
@@ -0,0 +1,19 @@
+import openpyxl
+
+
+def zip_list_excel(zip_name, zip_date, zip_quantity_docs):
+    try:
+        wb = openpyxl.load_workbook('./static/reestry/архивы_реестр.xlsx')
+        sheet = wb['Sheet']
+
+    except FileNotFoundError:
+        wb = openpyxl.Workbook()
+        sheet = wb['Sheet']
+        sheet['A1'] = 'Название архива'
+        sheet['B1'] = 'Дата формирования'
+        sheet['C1'] = 'Кол-во док-тов в архиве'
+        sheet['D1'] = 'Ссылка'
+
+    finally:
+        sheet.append([zip_name, zip_date, zip_quantity_docs])
+        wb.save(filename='./static/reestry/архивы_реестр.xlsx')
--- test/simpledms/documents/views.py
+++ test/simpledms/documents/views.py
@@ -3,6 +3,9 @@ from .serializers import DocumentSerializer
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters.rest_framework import FilterSet, filters
+from rest_framework.response import Response
+import datetime
+from . import travail_methods
 
 
 class DocumentListCreate(generics.ListCreateAPIView):
@@ -48,3 +51,15 @@ class DocumentListByDateFilter(generics.ListAPIView):
     serializer_class = DocumentSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = DocumentFilter
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        quantity_docs = len(queryset)
+        if quantity_docs > 0:
+            zip_date = datetime.date.today()
+            zip_name = zip_date.strftime("%d_%m_%Y")
+            travail_methods.zip_list_excel(zip_name, zip_date, quantity_docs)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
